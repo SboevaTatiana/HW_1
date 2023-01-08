2bc3857a0996c6689be29703f838d1c57dc40f19
Tatiana_S <Sbnuiiia@yandex.ru>
2023-01-09 02:12:56 +0300

    составлена структура проекта

---
+++ HW1/Ex08/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+int n = 5;
+int[] array = new int[n + 1];
+int index = 0;
+
+while (index < n + 1)
+{
+  if (index % 2 == 0)
+  {
+    array[index] = index;
+    Console.WriteLine(array[index]);
+  }
+  index++;
+}
