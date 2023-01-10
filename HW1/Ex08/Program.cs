a8e7903a10beb2510cfa4deff32a7157aea0c0ad
Tatiana_S <Sbnuiiia@yandex.ru>
2023-01-10 23:34:49 +0300

    Отредактирована задача 6 из первого ДЗ

--- HW1/Ex08/Program.cs
+++ HW1/Ex08/Program.cs
@@ -14,3 +14,27 @@ while (index < n + 1)
   }
   index++;
 }
+
+
+/*int i = 1;
+bool not = true;
+
+Console.WriteLine("Введите число:");
+num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (i <= num)
+{
+  if (i % 2 != 1)
+  {
+    Console.Write(i + ", ");
+    not = false;
+  }
+  i++;
+}
+
+if (not)
+{
+  Console.WriteLine("Нет чётных чисел!");
+}
+*/
