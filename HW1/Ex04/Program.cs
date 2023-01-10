34318949047be124b42d12f02676101217c93190
Tatiana_S <Sbnuiiia@yandex.ru>
2023-01-10 23:22:04 +0300

    Написана задача 4 из первого ДЗ

---
+++ HW1/Ex04/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите 3 числа:");
+int a1 = Convert.ToInt32(Console.ReadLine());
+int a2 = Convert.ToInt32(Console.ReadLine());
+int a3 = Convert.ToInt32(Console.ReadLine());
+
+int max = a1;
+
+if (a2 > max)
+{
+  max = a2;
+}
+
+if (a3 > max)
+{
+  max = a3;
+}
+
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
