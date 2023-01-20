9e6b6876f8d6cfb3e4837955c903a90edce642e5
Tatiana_S <Sbnuiiia@yandex.ru>
2023-01-20 15:08:24 +0300

    написаны функции и блок-схема задачи

---
+++ HW6/Ex41/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//-1, -7, 567, 89, 223-> 3
+
+using static Library;
+
+int number = Input();
+int[] array = CreateArray(number);
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(howManyNumbers(array));
