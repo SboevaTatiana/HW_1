ab529556af115f72750570e583ea0c25c852ee20
Tatiana_S <Sbnuiiia@yandex.ru>
2023-01-24 01:34:55 +0300

    написана программа и блок-схема задачи

---
+++ HW8/Ex56/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine("Введите размер массива m x n ");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int[,] array = new int[m, n];
+CreateArray(array);
+PrintArray(array);
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+Console.WriteLine("строка с наименьшей суммой элементов: " + (minSumLine + 1));
+
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i, 0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i, j];
+  }
+  return sumLine;
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(10);
+    }
+  }
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
